dd1f093fc10f138876c4c97ed3d61d9331530fdb
cchoirun <niamchoirun020#gmail.com>
2024-11-18 07:41:00 +0700

    Remove some useless assets

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,8 +4,8 @@ const CTA = () => {
   return (
     <section className='cta'>
       <p className='cta-text'>
-        Have a project in mind? <br className='sm:block hidden' />
-        Let’s build something together!
+        Ada pertanyaan? <br className='hidden sm:block' />
+        Hubungi Kami!
       </p>
       <Link to='/contact' className='btn'>
         Contact
